3bf654048d0dcd44e0c0a39a3251e00a6732b253
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-05-01 16:13:26 +0000

    updated routes.py with signup, login and get user

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from flask_jwt_extended import get_jwt_identity, create_access_token, jwt_required, create_access_token
+import hashlib
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +23,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def handle_signup():
+    body = request.get_json()
+    body_email = body["email"]
+    body_password = hashlib.sha256(body['password'].encode("utf-8")).hexdigest()
+    new_user = User(email = body_email, password = body_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify("User created"), 200
+    
+@api.route('/login', methods=['POST'])
+def handle_login():
+    body = request.get_json()
+    body_email = body["email"]
+    body_password = hashlib.sha256(body['password'].encode("utf-8")).hexdigest()
+    user = User.query.filter_by(email = body_email).first()
+    if user and user.password == body_password:
+        access_token = create_access_token(idenity = user.email)
+        return jsonify(access_token = access_token), 200
+    else:
+        return jsonify("User not found"), 400
+    
+@api.route('/user', methods=['GET'])
+@jwt_required()
+def handle_get_user():
+    user_email = get_jwt_identity()
+    user = User.query.filter_by(email = user_email).first()
+    return jsonify(user), 200
