041547b0657931ce3601b7da2cd484ec236bd9c7
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-05-05 14:40:47 +0000

    adjusted use effect to stay logged in

--- src/front/hooks/actions.js
+++ src/front/hooks/actions.js
@@ -40,8 +40,14 @@ export const getUser = async (dispatch, payload) => {
     );
     let data = await response.json();
 
+    // error handling
+    if (!response.ok) {
+        // If token is invalid, remove it and reset state
+        throw new Error("Invalid or expired token");
+    }
+
     dispatch({
         type: "set_user",
         payload: {user: data.user, access_token: payload}
     });
-}
+};
--- src/front/pages/Private.jsx
+++ src/front/pages/Private.jsx
@@ -1,30 +1,49 @@
 import React, { useEffect, useState } from "react"
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useNavigate } from "react-router-dom";
 
 export const Private = () => {
 
     const { store, dispatch, getUser} = useGlobalReducer()
     const [message, setMessage] = useState("")
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const token = sessionStorage.getItem('access_token');
+        if(token && !store.user) {
+            getUser(token)
+        }
+    }, [store.access_token])
 
     useEffect(() => {
-        getUser(store.access_token)
-    },[])
-
-    useEffect(() => {
-        const email = user.email;
-        if(store.user == null) {
+        if(store?.user == null) {
             setMessage("YOU MUST LOGIN FIRST")
         } 
         else {
-            setMessage(`Hello ${store.user.email}` )
+            setMessage(`Hello ${store.user.email}`)
         }
-    }, [store.user])
+    }, [])
 
-    
+    const handleLogout = () => {
+        sessionStorage.removeItem('access_token');
+
+        dispatch({
+            type: "set_user",
+            payload: { user: null, access_token: null },
+        });
+
+        navigate("/login");
+    }
 
     return (
         <div className="text-center mt-5">
-            {message}
+            <div>{message}</div>            
+            <button 
+                onClick={handleLogout} 
+                className="btn btn-danger mt-3">
+                    Log Out
+            </button>
         </div>
+        
     );
 }; 
